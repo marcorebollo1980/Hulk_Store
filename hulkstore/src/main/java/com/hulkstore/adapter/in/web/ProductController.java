d61b6345ddc5752cdd4ed6f8c638967f5500e914
Marco Antonio Rebollo Lopez <marcorebollolopez@gmail.com>
2024-05-30 11:30:26 -0500

    commit2

--- hulkstore/src/main/java/com/hulkstore/adapter/in/web/ProductController.java
+++ hulkstore/src/main/java/com/hulkstore/adapter/in/web/ProductController.java
@@ -7,6 +7,7 @@ import com.hulkstorea.application.port.in.RemoveProductUseCase;
 
 import jakarta.annotation.Nonnull;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +16,27 @@ import org.springframework.web.bind.annotation.*;
 @Nonnull
 @RequestMapping("/products")
 public class ProductController {
-    private static final AddProductUseCase addProductUseCase = null;
-    private static final RemoveProductUseCase removeProductUseCase = null;
-
+    //private static final AddProductUseCase addProductUseCase = null;
+    //private static final RemoveProductUseCase removeProductUseCase = null;
+  
+    
+    @Autowired
+    private AddProductUseCase addProductUseCase2;
+    
+    @Autowired
+    private RemoveProductUseCase removeProductUseCase2;
+    
     @PostMapping("/add")
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
-        return new ResponseEntity<>(addProductUseCase.addProduct(product), HttpStatus.CREATED);
+    	Product productOne = new Product();
+    	productOne.setId(product.getId());
+    	productOne.setName(product.getName());
+        return new ResponseEntity<>(addProductUseCase2.addProduct(productOne), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> removeProduct(@PathVariable Long id) {
-        removeProductUseCase.removeProduct(id);
+    	removeProductUseCase2.removeProduct(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }	
     
--- hulkstore/src/main/java/com/hulkstore/domain/model/Product.java
+++ hulkstore/src/main/java/com/hulkstore/domain/model/Product.java
@@ -1,11 +1,19 @@
 package com.hulkstore.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Product {
+@Entity
+public class Product { 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int quantity;
     private double price;
+    
 	/**
 	 * @return the id
 	 */
